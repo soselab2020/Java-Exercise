f10668d850eb193b48d3464de33260a79ce260cd
albertma2020 <58982777+albertma2020@users.noreply.github.com>
2024-05-07 22:50:52 +0800

    feat: 增加貝茲的資訊

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,6 +3,7 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"12345678\"\n姓名：\"谷翔平\"");
+        System.out.println( "\"學號：\"87654321\"\n姓名：\"貝茲\""); 
     } // end method main
 
 } // end class FirstClass

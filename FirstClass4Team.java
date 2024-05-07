d3df0f14baafd2f54b1d786f8806b6cbe7129a31
SOSE Lab <shangpinma@gmail.com>
2024-05-07 22:34:17 +0800

    feat: 調整排版

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -1,10 +1,8 @@
-public class FirstClass4Team
-{
+public class FirstClass4Team {
     // main method begins execution of Java application
-    public static void main( String[] args )
-    {
-        System.out.println( "這是用來練習GitHub協作的程式!" );
-        System.out.println( "\"學號：\"12345678\"\n姓名：\"谷翔平\"");        
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"12345678\"\n姓名：\"谷翔平\"");
     } // end method main
 
 } // end class FirstClass

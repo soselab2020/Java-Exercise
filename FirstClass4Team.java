2adb97acaebe2d1dd132ce6ada95f5a85da1e5ca
SOSE Lab <shangpinma@gmail.com>
2024-05-07 22:32:48 +0800

    feat: 增加谷翔平的資訊

---
+++ FirstClass4Team.java
@@ -0,0 +1,10 @@
+public class FirstClass4Team
+{
+    // main method begins execution of Java application
+    public static void main( String[] args )
+    {
+        System.out.println( "這是用來練習GitHub協作的程式!" );
+        System.out.println( "\"學號：\"12345678\"\n姓名：\"谷翔平\"");        
+    } // end method main
+
+} // end class FirstClass

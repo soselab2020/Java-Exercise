b9dd022da8e1cc19c2d19b4271c6eefc61cd73ac
soselab2020 <shangpinma@gmail.com>
2024-05-08 15:43:46 +0800

    feat: add a new line

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,6 +2,7 @@ public class FirstClass4Team {
     // main method begins execution of Java application
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("我是大鼓!");
     } // end method main
 
 } // end class FirstClass

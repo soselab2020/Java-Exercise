75890e6244ddce1888c9830898227a69e8115de4
SOSE Lab <shangpinma@gmail.com>
2024-05-08 10:19:46 +0800

    fix: remove two items

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,8 +2,6 @@ public class FirstClass4Team {
     // main method begins execution of Java application
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
-        System.out.println("\"學號：\"12345678\"\n姓名：\"谷翔平\"");
-        System.out.println( "\"學號：\"87654321\"\n姓名：\"貝茲\""); 
     } // end method main
 
 } // end class FirstClass
